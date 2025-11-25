ea695c5959e55b3c17090d4d373193cf8a8abe42
Olga Andreeva <volha.mashnitskaya@gmail.com>
2025-11-25 17:19:11 +0100

    chor: add test

--- src/app/features/city-details/weather/weather.test.tsx
+++ src/app/features/city-details/weather/weather.test.tsx
@@ -1,10 +1,22 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import { act } from 'react';
+
 import { Weather } from '../weather';
 import { TestWrapper } from 'src/app/tests/mocks/ui/render';
 import { makeStore, TestStore } from 'src/app/tests/mocks/redux/store';
-import { act } from 'react';
+import { selectCity } from 'src/app/store';
 
 describe('Weather', () => {
+  const mockWeatherData = {
+    current_weather: {
+      interval: 900,
+      temperature: 22.5,
+      windspeed: 15.2,
+      winddirection: 180,
+      weathercode: 1,
+      is_day: 1,
+    },
+  };
   let store: TestStore;
 
   beforeEach(() => {
@@ -53,5 +65,49 @@ describe('Weather', () => {
         screen.getByText(/Error loading weather data/i)
       ).toBeInTheDocument();
     });
+  });
+
+  it('should display weather data when fetch succeeds', async () => {
+    (global.fetch as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve(mockWeatherData),
+    });
+
+    await act(async () => {
+      store.dispatch(selectCity('amsterdam'));
+
+      renderComponent();
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole('table', { name: /weather table/i })
+      ).toBeInTheDocument();
+    });
+
+    expect(screen.getByText('Temperature')).toBeInTheDocument();
+    expect(screen.getByText('22.5 °C')).toBeInTheDocument();
+
+    expect(screen.getByText('Wind Speed')).toBeInTheDocument();
+    expect(screen.getByText('15.2 km/h')).toBeInTheDocument();
+
+    expect(screen.getByText('Wind Direction')).toBeInTheDocument();
+    expect(screen.getByText('180°')).toBeInTheDocument();
+
+    expect(screen.getByText('Weather Code')).toBeInTheDocument();
+    expect(screen.getByText('1')).toBeInTheDocument();
+
+    expect(screen.getByText('Interval')).toBeInTheDocument();
+    expect(screen.getByText('900 seconds')).toBeInTheDocument();
+
+    expect(screen.getByText('Day / Night')).toBeInTheDocument();
+    expect(screen.getByText('Day')).toBeInTheDocument();
+
+    expect(
+      screen.queryByText('Loading weather data...')
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText('Error loading weather data')
+    ).not.toBeInTheDocument();
   });
 });
